f2adc94a39fe0ad599e76765aa178ff0c7ffd014
ㅤYV55 <yassine.ouzzane@etu.utc.fr>
2024-05-09 23:21:12 +0200

    Modifications mineures pour la lisibilté

--- Rendu Semaine N°4/Application Python/emetLS.py
+++ Rendu Semaine N°4/Application Python/emetLS.py
@@ -1,23 +1,23 @@
 import psycopg2
 
 def insert_émetLS(cur, conn):
-    signalement = input("Insérer le numéro d'immatricalation du véhicule :")
-    locataire = input("Insérer le pseudo du locataire : ")
+    signalement = input("Insérer le numéro d'immatricalation du véhicule :"\n)
+    locataire = input("Insérer le pseudo du locataire : "\n)
     query = f"INSERT INTO ÉmetLS (signalement, locataire) VALUES ('{signalement}', '{locataire}')"
     cur.execute(query)
     conn.commit()
     print("Le signalement a été effectué avec succès.\n")
 
 def update_émetLS(cur, conn):
-    signalement = input("Insérer le numéro d'immatriculation à mettre à jour :")
+    signalement = input("Insérer le numéro d'immatriculation à mettre à jour :"\n)
     querylocataire = f"UPDATE ÉmetLS SET locataire = '{new_locataire}' WHERE signalement = '{signalement}'"
     cur.execute(querylocataire)
     conn.commit()
-    print("Le signalement a été mis à jour avec succès.")
+    print("Le signalement a été mis à jour avec succès."\n)
 
 def delete_émetLS(cur, conn):
-    signalement = input("Insérer le numéro d'immatriculation à supprimer :")
+    signalement = input("Insérer le numéro d'immatriculation à supprimer :"\n)
     querylocataire = f"DELETE FROM ÉmetLS WHERE signalement = '{signalement}'"
     cur.execute(querylocataire)
     conn.commit()
-    print("Le signalement a été supprimé avec succès.")
+    print("Le signalement a été supprimé avec succès."\n)
